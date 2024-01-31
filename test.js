6639b400afa6887e20336c64acb5aace4184faef
J <111350262+Jagadsw@users.noreply.github.com>
2024-01-31 18:27:40 +0700

    Test

---
+++ test.js
@@ -0,0 +1,23 @@
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname) {
+    for (tablink of tablinks) {
+        tablink.classList.remove("active-tab");
+    }
+
+    for (tabcontent of tabcontents) {
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+}
+
+var sidemenus = document.getElementById("sidemenu");
+
+function openmenu() {
+    sidemenus.style.right = "0";
+}
+
+function closemenu() {
+    sidemenus.style.right = "-200px";
+}
